cd11a27f4a5b1fe951b5e72dfd31a62c40d709ca
ndelanhese <nnathan3@gmail.com>
2023-04-12 11:13:56 -0300

    feat(lint and husky): added lint and husky on project

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    '@rocketseat/eslint-config/node',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -17,9 +18,56 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        useTabs: false,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+    ],
+    'no-use-before-define': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-alert': 'error',
+    'no-restricted-imports': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: 'InputData$',
+      },
+    ],
+    'no-undef': 'error',
+    'no-empty': 'error',
+    '@typescript-eslint/no-empty-function': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          '{}': false,
+          object: false,
+          Function: true,
+          Object: false,
+        },
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ['external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
 };
